e37103887e7cdda6547db403ca42918eda5c27dc
jin-young <wladuddk1385@gmail.com>
2026-04-04 13:46:31 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/recipe_side/RecipeSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipe_side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSideApplication.class, args);
+	}
+
+}
