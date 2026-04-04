ec2330f467f3ac512ee3243c2df3bcaa1533019b
jin-young <wladuddk1385@gmail.com>
2026-04-04 16:06:10 +0900

    패키지 구조 생성

---
+++ src/main/java/com/nxtstp/nststpApplication.java
@@ -0,0 +1,11 @@
+package com.nxtstp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class nststpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(nststpApplication.class, args);
+    }
+}
